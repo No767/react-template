8462534ac2a3177f35ee6213a59e755f6f3654dc
No767 <73260931+No767@users.noreply.github.com>
2025-02-09 19:26:48 -0800

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import "./App.tsx";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-
 import App from "./App.tsx";
-import { routeTree } from "./routeTree.gen";
 
 // biome-ignore lint/style/noNonNullAssertion: Provided by React
 const rootElement = document.getElementById("root")!;
